c652a6e6ae1121d59255e064a4a01e9444872980
apautvein <apautvein@gmail.com>
2020-10-11 21:20:35 -0500

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Security.Policy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -13,6 +16,54 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            CelestialObject celestialObject = new CelestialObject();
+            celestialObject.Name = "GetById";
+            if (celestialObject.OrbitedObjectId == id)
+            {
+                celestialObject.Satellites.Add(celestialObject);  
+            }
+            if (celestialObject.Id != id)
+            {
+                return NotFound(); 
+            } else
+            {
+                return Ok(); 
+            }
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            CelestialObject celestialObject = new CelestialObject();
+            if (celestialObject.OrbitedObjectId == celestialObject.Id)
+            {
+                celestialObject.Satellites.Add(celestialObject);
+            }
+            if (celestialObject.Name != name)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            CelestialObject celestialObject = new CelestialObject();
+            if (celestialObject.OrbitedObjectId == celestialObject.Id)
+            {
+                celestialObject.Satellites.Add(celestialObject);
+            }
+                return Ok();
+           
         }
     }
 }
